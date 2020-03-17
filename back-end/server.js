6daa3099f5693fb304becdcc697155de96cccfb6
Ioannis Pelekoudas <peleioannis@gmail.com>
2020-03-17 15:54:50 +0200

    SSL

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,17 @@
 // import packages
 const express = require('express');
+const fs = require("fs");
+const https = require("https");
 
 // import files
 const users = require('./routes/users');
 const incidents = require('./routes/incidents');
 
 // declare vars
+const options = {
+	key: fs.readFileSync("./server.key"),
+	cert: fs.readFileSync("./server.crt")
+};
 const app = express();
 const port = 3000;
 
@@ -13,6 +19,8 @@ app.use('/users', users);
 app.use('/incidents', incidents);
 
 
-app.listen(port, function(){
-	console.log(`Server started on port ${port}`);
-})
+// app.listen(port, function(){
+// 	console.log(`Server started on port ${port}`);
+// })
+
+https.createServer(options, app).listen(port);
