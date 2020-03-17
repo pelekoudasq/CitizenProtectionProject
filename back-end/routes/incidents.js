28dd75d34ed97ab61877b95d34e382a4804cb26f
TheoKant <kantartzistheophilos@gmail.com>
2020-03-17 20:58:56 +0200

    get incident by priority method

--- back-end/routes/incidents.js
+++ back-end/routes/incidents.js
@@ -26,7 +26,7 @@ router.get('/all', function(req, res, next) {
 
 //get incident by id
 router.get('/:id', function(req, res, next) {
-	console.log('incidents: get by id');
+	console.log('incidents: get by id' );
 	db.Incidents.findOne({ _id: mongojs.ObjectID(req.params.id) }, function(err, incident) {
 		if (err) {
 			res.send(err);
@@ -35,6 +35,19 @@ router.get('/:id', function(req, res, next) {
 		res.json(incident);
 	})
 })
+
+//get incident by priority
+router.get('/priority/:priority', function(req, res, next) {
+	console.log('incidents: get by priority ' );
+	db.Incidents.findOne({ priority: req.params.priority }, function(err, incident) {
+		if (err) {
+			res.send(err);
+			return;
+		}
+		res.json(incident);
+	})
+})
+
 
 
 module.exports = router;
