7ced72d3a19d7b7cf65d27394e1aaab06ccd5219
VasiaKoum <vasiakoum@hotmail.com>
2020-03-26 20:43:01 +0200

    add files

--- front-end/cpp_sw/src/App.js
+++ front-end/cpp_sw/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import mainlogo from './mainlogo.png';
+// import NavBar from './NavBar'
+import { Button, Form, FormGroup, Label, Input , Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload, kalispera.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import { FacebookLoginButton } from 'react-social-login-buttons';
+
+
+class App extends Component{
+  render(){
+    return (
+    <div>
+      <nav className="navbar navbar-dark mb-3">
+        <a className="navbar-brand" href="#">
+        <h1><img src={mainlogo} className="App-logo" alt="logo" /> <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
         </a>
-      </header>
-    </div>
-  );
+      </nav>
+       <Form className="login-form">
+          <h1>
+           <span className="font-weight-bold">here</span>.com
+          </h1>
+          <h2 className="text-center">Welcome</h2>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input type="name" placeholder="Όνομα χρήστη"/>
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input type="password" placeholder="Κωδικός χρήστη"/>
+          </FormGroup>
+          <Button className="btn-lg btn-dark btn-block">Log in</Button>
+          <div className="text-center pt-3">rrrr </div>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ front-end/cpp_sw/src/navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class NavBar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark mb-3">
+          <a className="navbar-brand" href="#">
+            <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+          </a>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default NavBar;
