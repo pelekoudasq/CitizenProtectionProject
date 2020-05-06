5856372092dcf3e19b3ec45a886b684f93b9e114
dimpapac <demispapa@gmail.com>
2020-05-06 20:01:00 +0300

    {token && <Logout />}

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -10,53 +10,30 @@ class NavBar extends Component
     render()    
     {
         let token = localStorage.getItem("token")
-        if (token !== null)
-        {
-            return (
-                <React.Fragment>
-                    <nav className="navbar navbar-dark mb-3">
-                        <a className="navbar-brand" href="/">
-                        <img className="logo_img"
-                            src={logo}
-                            alt=''
-                        />
-                        <h1>
-                            <span className="badge badge-secondary">{this.props.totalItems}</span>
-                        </h1>
-                        </a>
-                        <div className="navbar_line"></div>
-                        <h5 className = "navbar_text">
-                            Πλατφόρμα Προστασίας Πολίτη
-                        </h5>
-                        <div>
-                        <Logout />
-                        </div>
-                    </nav>
-                </React.Fragment>
-            );
-        }
-        else 
-        {
-            return (
-                <React.Fragment>
-                    <nav className="navbar navbar-dark mb-3">
-                        <a className="navbar-brand" href="/">
-                        <img className="logo_img"
-                            src={logo}
-                            alt=''
-                        />
-                        <h1>
-                            <span className="badge badge-secondary">{this.props.totalItems}</span>
-                        </h1>
-                        </a>
-                        <div className="navbar_line"></div>
-                        <h5 className = "navbar_text">
-                            Πλατφόρμα Προστασίας Πολίτη
-                        </h5>
-                    </nav>
-                </React.Fragment>
-            );
-        }
+
+        return (
+            <React.Fragment>
+                <nav className="navbar navbar-dark mb-3">
+                    <a className="navbar-brand" href="/">
+                    <img className="logo_img"
+                        src={logo}
+                        alt=''
+                    />
+                    <h1>
+                        <span className="badge badge-secondary">{this.props.totalItems}</span>
+                    </h1>
+                    </a>
+                    <div className="navbar_line"></div>
+                    <h5 className = "navbar_text">
+                        Πλατφόρμα Προστασίας Πολίτη
+                    </h5>
+                    <div>
+                    {token && <Logout />}
+
+                    </div>
+                </nav>
+            </React.Fragment>
+        );
     }
 }
 
